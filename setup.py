a152bd734e999bcd00ac1893108b136936291bf5
Şefik Serangil <sefik.serengil@softtech.com.tr>
2020-04-09 17:12:04 +0300

    opencv

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="deepface",  
-    version="0.0.15",
+    version="0.0.16",
     author="Sefik Ilkin Serengil",
     author_email="serengil@gmail.com",
     description="Deep Face Analysis Framework for Face Recognition and Demography",
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5.5',
-    install_requires=["numpy>=1.14.0", "pandas>=0.23.4", "tqdm>=4.30.0", "gdown>=3.10.1", "opencv-python>=3.4.4", "Pillow>=5.2.0", "tensorflow>=1.9.0", "keras>=2.2.0"]
+    install_requires=["numpy>=1.14.0", "pandas>=0.23.4", "tqdm>=4.30.0", "gdown>=3.10.1", "Pillow>=5.2.0", "tensorflow>=1.9.0", "keras>=2.2.0"]
 )
