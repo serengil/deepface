dcd883f223339958017b07e489a1b794ed17408a
Sefik Ilkin Serengil <serengil@gmail.com>
2021-06-24 16:11:07 +0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="deepface",
-    version="0.0.61",
+    version="0.0.62",
     author="Sefik Ilkin Serengil",
     author_email="serengil@gmail.com",
     description="A Lightweight Face Recognition and Facial Attribute Analysis Framework (Age, Gender, Emotion, Race) for Python",
