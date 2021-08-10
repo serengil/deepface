3dcd64aac75f0f710dfa0176793ad9a2910a9674
Xu-jianwen <18310606913@163.com>
2021-08-10 22:32:02 +0800

    -

--- tests/stream.py
+++ tests/stream.py
@@ -1,8 +1,8 @@
 from deepface import DeepFace
 
-DeepFace.stream("dataset") #opencv
+# DeepFace.stream("dataset") #opencv
 #DeepFace.stream("dataset", detector_backend = 'opencv')
-#DeepFace.stream("dataset", detector_backend = 'ssd')
-#DeepFace.stream("dataset", detector_backend = 'mtcnn')
+DeepFace.stream("D:/face/320_database/", detector_backend = 'ssd', enable_face_analysis=False)
+# DeepFace.stream("D:/face/320_database/", detector_backend = 'mtcnn')
 #DeepFace.stream("dataset", detector_backend = 'dlib')
-#DeepFace.stream("dataset", detector_backend = 'retinaface')
+# DeepFace.stream("D:/face/320_database/", detector_backend = 'retinaface')
