897d9d13ef04353f11a89033fb1e973841cbd5d9
Sefik Ilkin Serengil <serengil@gmail.com>
2023-02-01 19:02:52 +0000

    api with gunicorn

--- setup.py
+++ setup.py
@@ -16,12 +16,25 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
     ],
     entry_points={
-        "console_scripts":
-        ["deepface = deepface.DeepFace:cli"],
+        "console_scripts": ["deepface = deepface.DeepFace:cli"],
     },
-    python_requires='>=3.5.5',
-    install_requires=["numpy>=1.14.0", "pandas>=0.23.4", "tqdm>=4.30.0", "gdown>=3.10.1", "Pillow>=5.2.0", "opencv-python>=4.5.5.64", "tensorflow>=1.9.0", "keras>=2.2.0", "Flask>=1.1.2", "mtcnn>=0.1.0", "retina-face>=0.0.1", "fire>=0.4.0", "gunicorn>=60.9.1"]
+    python_requires=">=3.5.5",
+    install_requires=[
+        "numpy>=1.14.0",
+        "pandas>=0.23.4",
+        "tqdm>=4.30.0",
+        "gdown>=3.10.1",
+        "Pillow>=5.2.0",
+        "opencv-python>=4.5.5.64",
+        "tensorflow>=1.9.0",
+        "keras>=2.2.0",
+        "Flask>=1.1.2",
+        "mtcnn>=0.1.0",
+        "retina-face>=0.0.1",
+        "fire>=0.4.0",
+        "gunicorn>=20.1.0",
+    ],
 )
