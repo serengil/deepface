d35833e4e1d9b70254c64fb04ebc3a3f2cd8d8ca
Sefik Ilkin Serengil <serengil@gmail.com>
2024-01-08 17:31:49 +0000

    some more check for find test

--- tests/test_find.py
+++ tests/test_find.py
@@ -7,19 +7,42 @@ logger = Logger("tests/test_find.py")
 
 
 def test_find_with_exact_path():
-    dfs = DeepFace.find(img_path="dataset/img1.jpg", db_path="dataset", silent=True)
+    img_path = "dataset/img1.jpg"
+    dfs = DeepFace.find(img_path=img_path, db_path="dataset", silent=True)
+    assert len(dfs) > 0
     for df in dfs:
         assert isinstance(df, pd.DataFrame)
+
+        # one is img1.jpg itself
+        identity_df = df[df["identity"] == img_path]
+        assert identity_df.shape[0] > 0
+
+        # validate reproducability
+        assert identity_df["VGG-Face_cosine"].values[0] == 0
+
+        df = df[df["identity"] != img_path]
         logger.debug(df.head())
         assert df.shape[0] > 0
     logger.info("✅ test find for exact path done")
 
 
 def test_find_with_array_input():
-    img1 = cv2.imread("dataset/img1.jpg")
+    img_path = "dataset/img1.jpg"
+    img1 = cv2.imread(img_path)
     dfs = DeepFace.find(img1, db_path="dataset", silent=True)
-
+    assert len(dfs) > 0
     for df in dfs:
+        assert isinstance(df, pd.DataFrame)
+
+        # one is img1.jpg itself
+        identity_df = df[df["identity"] == img_path]
+        assert identity_df.shape[0] > 0
+
+        # validate reproducability
+        assert identity_df["VGG-Face_cosine"].values[0] == 0
+
+
+        df = df[df["identity"] != img_path]
         logger.debug(df.head())
         assert df.shape[0] > 0
 
