3690f329496e27ec003efc03f1a5d26c9a967ed7
Sefik Ilkin Serengil <serengil@gmail.com>
2024-02-04 17:27:17 +0000

    add option to use flak run in service sh

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 cd ../deepface/api/src
+
+# run the service with flask - not for production purposes
+# python api.py
+
+# run the service with gunicorn - for prod purposes
 gunicorn --workers=1 --timeout=3600 --bind=0.0.0.0:5000 "app:create_app()"
