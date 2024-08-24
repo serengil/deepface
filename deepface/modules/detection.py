cc8c3f0461a253de512c2cdf39b35e16d382e9bd
Josh <joshua@caption-pro.com>
2024-08-24 09:07:53 +0100

    Fixed rotation angle

--- deepface/modules/detection.py
+++ deepface/modules/detection.py
@@ -329,7 +329,7 @@ def align_img_wrt_eyes(
 
     (h, w) = img.shape[:2]
     center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, -angle, 1.0)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
     img = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT, borderValue=(0,0,0))
 
     return img, angle
