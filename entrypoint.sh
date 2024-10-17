9c5981f8a64f39b905010ed00b8e4f8feb3dee33
seavphing <seavphing789@gmail.com>
2024-10-17 10:24:56 +0700

    correct docker file and improve requirements.txt file to use the workable tensorflow version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,5 @@
-gunicorn -w 1 -b 0.0.0.0:5000 --timeout 7200 --log-level 'debug' --access-logfile - --access-logformat '%(h)s - - [%(t)s] "%(r)s" %(s)s %(b)s %(L)s' app:create_app()
+# Example content
+echo "Starting the application..."
+exec "$@"
+
+gunicorn -w 1 -b 0.0.0.0:5000 --timeout 7200 --log-level 'debug' --access-logfile - --access-logformat '%(h)s - - [%(t)s] "%(r)s" %(s)s %(b)s %(L)s' app:create_app
