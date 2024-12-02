e73bdb813801e91cc54c016ffc651daa138ea922
Sefik Ilkin Serengil <serengil@gmail.com>
2024-12-02 12:39:06 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 echo "Starting the application..."
 exec "$@"
 
-gunicorn -w 1 -b 0.0.0.0:5000 --timeout 7200 --log-level 'debug' --access-logfile - --access-logformat '%(h)s - - [%(t)s] "%(r)s" %(s)s %(b)s %(L)s' app:create_app
+gunicorn --workers=1 --timeout=7200 --bind=0.0.0.0:5000 --log-level=debug --access-logformat='%(h)s - - [%(t)s] "%(r)s" %(s)s %(b)s %(L)s' --access-logfile=- "app:create_app()"
