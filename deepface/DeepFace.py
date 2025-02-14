01f872d9e9b2052dbd787a6a2c19b052ba6aba22
Nat Lee <natlee.work@gmail.com>
2025-02-14 15:52:48 +0800

    [update] mv control logic to `demography`

--- deepface/DeepFace.py
+++ deepface/DeepFace.py
@@ -256,29 +256,6 @@ def analyze(
             - 'middle eastern': Confidence score for Middle Eastern ethnicity.
             - 'white': Confidence score for White ethnicity.
     """
-
-    if isinstance(img_path, np.ndarray) and len(img_path.shape) == 4:
-        # Received 4-D array, which means image batch.
-        # Check batch dimension and process each image separately.
-        if img_path.shape[0] > 1:
-            batch_resp_obj = []
-            # Execute analysis for each image in the batch.
-            for single_img in img_path:
-                resp_obj = demography.analyze(
-                    img_path=single_img,
-                    actions=actions,
-                    enforce_detection=enforce_detection,
-                    detector_backend=detector_backend,
-                    align=align,
-                    expand_percentage=expand_percentage,
-                    silent=silent,
-                    anti_spoofing=anti_spoofing,
-                )
-
-                # Append the response object to the batch response list.
-                batch_resp_obj.append(resp_obj)
-            return batch_resp_obj
-
     return demography.analyze(
         img_path=img_path,
         actions=actions,
