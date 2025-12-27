0528793a90993c8b0a8be53988d8f44b958ba20e
Sefik Ilkin Serengil <serengil@gmail.com>
2025-12-27 13:18:35 +0000

    allow users to load models on app start up to respond faster in 1st request (#1548)

--- deepface/api/src/dependencies/variables.py
+++ deepface/api/src/dependencies/variables.py
@@ -5,5 +5,7 @@ import os
 # pylint: disable=too-few-public-methods
 class Variables:
     def __init__(self) -> None:
-        self.database_type = os.getenv("DATABASE_TYPE", "postgres").lower()
+        self.database_type = os.getenv("DEEPFACE_DATABASE_TYPE", "postgres").lower()
         self.conection_details = os.getenv("DEEPFACE_CONNECTION_DETAILS")
+        self.face_recognition_models = os.getenv("DEEPFACE_FACE_RECOGNITION_MODELS")
+        self.face_detection_models = os.getenv("DEEPFACE_FACE_DETECTION_MODELS")
